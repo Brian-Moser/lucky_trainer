771a762b1d0d148196d74aa85eabe83b65ac9018
Brian Moser <brian.moser@dfki.de>
2019-11-24 15:05:15 +0100

    lets plot gradients

--- trainer.py
+++ trainer.py
@@ -31,8 +31,11 @@ def plot_grad_flow(named_parameters, title, t):
     layers = []
     for n, p in named_parameters:
         if(p.requires_grad) and ("bias" not in n):
-            layers.append(n)
-            ave_grads.append(p.grad.abs().mean().cpu().data[0])
+            try:
+                ave_grads.append(p.grad.abs().mean().cpu().data[0])
+                layers.append(n)
+            except:
+                pass
     plt.plot(ave_grads, alpha=0.3, color="b")
     plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
     plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
