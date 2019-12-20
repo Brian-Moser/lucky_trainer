9cdf1bc09a3008a41ffb77be5f949c5a74b98db9
Brian Moser <brian.moser@dfki.de>
2019-12-20 18:12:24 +0100

    test

--- misc/custom_dataset_classes.py
+++ misc/custom_dataset_classes.py
@@ -435,7 +435,7 @@ class H5Dataset(Dataset):
         #    self.images = f['images'][:]
             input = f['images'][index]
             labels = f['labels'][index]
-            print(self.f['labels'][index], labels)
+            print(self.f['labels'][index].value, labels)
         return self.transform(input.astype('float32')), np.array(labels.item(), dtype=np.int64)
 
     def __len__(self):
